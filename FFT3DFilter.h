5e79e690c38b5ae290491af3d6736a199c3107a1
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2020-08-06 14:53:50 +0200

    Eliminate forward declarations

--- FFT3DFilter.h
+++ FFT3DFilter.h
@@ -56,92 +56,6 @@ void ApplyPattern3D3_degrid_C(fftwf_complex *out, const fftwf_complex *outprev, 
 void ApplyPattern3D4_degrid_C(fftwf_complex *out, const fftwf_complex *outprev2, const fftwf_complex *outprev, const fftwf_complex *outnext, int outwidth, int outpitchelems, int bh, int howmanyblocks, const float *pattern3d, float beta, float degrid, const fftwf_complex *gridsample);
 void ApplyPattern3D5_degrid_C(fftwf_complex *out, const fftwf_complex *outprev2, const fftwf_complex *outprev, const fftwf_complex *outnext, const fftwf_complex *outnext2, int outwidth, int outpitchelems, int bh, int howmanyblocks, const float *pattern3d, float beta, float degrid, const fftwf_complex *gridsample);
 
-class FFT3DFilterTransform;
-
-class FFT3DFilter
-{
-private:
-    /* parameters */
-    float sigma;    /* noise level (std deviation) for high frequncies */
-    float beta;     /* relative noise margin for Wiener filter */
-    int   plane;    /* color plane */
-    int   bw;       /* block width */
-    int   bh;       /* block height */
-    int   bt;       /* block size  along time (mumber of frames), =0 for Kalman, >0 for Wiener */
-    int   ow;       /* overlap width - v.0.9 */
-    int   oh;       /* overlap height - v.0.9 */
-    float kratio;   /* threshold to sigma ratio for Kalman filter */
-    float sharpen;  /* sharpen factor (0 to 1 and above) */
-    float scutoff;  /* sharpen cufoff frequency (relative to max) - v1.7 */
-    float svr;      /* sharpen vertical ratio (0 to 1 and above) - v.1.0 */
-    float smin;     /* minimum limit for sharpen (prevent noise amplifying) - v.1.1 */
-    float smax;     /* maximum limit for sharpen (prevent oversharping) - v.1.1 */
-    int   pframe;   /* noise pattern frame number */
-    int   px;       /* noise pattern window x-position */
-    int   py;       /* noise pattern window y-position */
-    float pfactor;  /* noise pattern denoise strength */
-    float sigma2;   /* noise level for middle frequencies */
-    float sigma3;   /* noise level for low frequencies */
-    float sigma4;   /* noise level for lowest (zero) frequencies */
-    float degrid;   /* decrease grid */
-    float dehalo;   /* remove halo strength - v.1.9 */
-    float hr;       /* halo radius - v1.9 */
-    float ht;       /* halo threshold - v1.9 */
-
-    /* additional parameterss */
-
-    const VSFrameRef *gridsample;
-    int nox, noy;
-    int outwidth;
-    int outpitch;
-    int outpitchelems; /* v.1.7 */
-
-    int outsize;
-    int howmanyblocks;
-
-    std::unique_ptr<float[], decltype(&fftw_free)> wsharpen;
-    std::unique_ptr<float[], decltype(&fftw_free)> wdehalo;
-
-    // Shared buffers only used for bt=0 (kalman) mode
-    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> outLast;
-    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> covar;
-    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> covarProcess;
-    //
-    float sigmaSquaredNoiseNormed;
-    float sigmaSquaredNoiseNormed2D;
-    float sigmaSquaredSharpenMinNormed;
-    float sigmaSquaredSharpenMaxNormed;
-    float ht2n; /* halo threshold squared normed */
-    float norm; /* normalization factor */
-
-    std::unique_ptr<float[], decltype(&fftw_free)> pattern2d;
-    std::unique_ptr<float[], decltype(&fftw_free)> pattern3d;
-
-    const VSVideoInfo *vi;
-    VSNodeRef *node;
-
-    template < int btcur >
-    void Wiener3D( int n, VSNodeRef *node, VSFrameRef *dst, VSFrameContext *frame_ctx, const VSAPI *vsapi );
-
-public:
-    VSFrameRef *ApplyFilter(int n, VSFrameContext *frame_ctx, VSCore *core, const VSAPI *vsapi);
-
-    /* Constructor */
-    FFT3DFilter
-    (
-        FFT3DFilterTransform *transform, const VSVideoInfo *vi,
-        float _sigma, float _beta, int _plane, int _bw, int _bh, int _bt, int _ow, int _oh,
-        float _kratio, float _sharpen, float _scutoff, float _svr, float _smin, float _smax,
-        int _pframe, int _px, int _py, bool _pshow, float _pcutoff, float _pfactor,
-        float _sigma2, float _sigma3, float _sigma4, float _degrid,
-        float _dehalo, float _hr, float _ht, int _ncpu,
-        VSNodeRef *node, VSCore *core, const VSAPI *vsapi
-    );
-
-    static const VSFrameRef *VS_CC GetFrame(int n, int activation_reason, void *instance_data, void **frame_data, VSFrameContext *frame_ctx, VSCore *core, const VSAPI *vsapi);
-    static void VS_CC Free(void *instance_data, VSCore *core, const VSAPI *vsapi);
-};
-
 class FFT3DFilterTransform {
 private:
     /* parameters */
@@ -267,6 +181,89 @@ private:
 public:
     FFT3DFilterPShow(VSNodeRef *node, int plane, int bw, int bh, int ow, int oh, bool interlaced, VSCore *core, const VSAPI *vsapi);
     VSFrameRef *GetFrame(const VSFrameRef *src, VSCore *core, const VSAPI *vsapi);
+
+    static const VSFrameRef *VS_CC GetFrame(int n, int activation_reason, void *instance_data, void **frame_data, VSFrameContext *frame_ctx, VSCore *core, const VSAPI *vsapi);
+    static void VS_CC Free(void *instance_data, VSCore *core, const VSAPI *vsapi);
+};
+
+class FFT3DFilter {
+private:
+    /* parameters */
+    float sigma;    /* noise level (std deviation) for high frequncies */
+    float beta;     /* relative noise margin for Wiener filter */
+    int   plane;    /* color plane */
+    int   bw;       /* block width */
+    int   bh;       /* block height */
+    int   bt;       /* block size  along time (mumber of frames), =0 for Kalman, >0 for Wiener */
+    int   ow;       /* overlap width - v.0.9 */
+    int   oh;       /* overlap height - v.0.9 */
+    float kratio;   /* threshold to sigma ratio for Kalman filter */
+    float sharpen;  /* sharpen factor (0 to 1 and above) */
+    float scutoff;  /* sharpen cufoff frequency (relative to max) - v1.7 */
+    float svr;      /* sharpen vertical ratio (0 to 1 and above) - v.1.0 */
+    float smin;     /* minimum limit for sharpen (prevent noise amplifying) - v.1.1 */
+    float smax;     /* maximum limit for sharpen (prevent oversharping) - v.1.1 */
+    int   pframe;   /* noise pattern frame number */
+    int   px;       /* noise pattern window x-position */
+    int   py;       /* noise pattern window y-position */
+    float pfactor;  /* noise pattern denoise strength */
+    float sigma2;   /* noise level for middle frequencies */
+    float sigma3;   /* noise level for low frequencies */
+    float sigma4;   /* noise level for lowest (zero) frequencies */
+    float degrid;   /* decrease grid */
+    float dehalo;   /* remove halo strength - v.1.9 */
+    float hr;       /* halo radius - v1.9 */
+    float ht;       /* halo threshold - v1.9 */
+
+    /* additional parameterss */
+
+    const VSFrameRef *gridsample;
+    int nox, noy;
+    int outwidth;
+    int outpitch;
+    int outpitchelems; /* v.1.7 */
+
+    int outsize;
+    int howmanyblocks;
+
+    std::unique_ptr<float[], decltype(&fftw_free)> wsharpen;
+    std::unique_ptr<float[], decltype(&fftw_free)> wdehalo;
+
+    // Shared buffers only used for bt=0 (kalman) mode
+    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> outLast;
+    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> covar;
+    std::unique_ptr<fftwf_complex[], decltype(&fftw_free)> covarProcess;
+    //
+    float sigmaSquaredNoiseNormed;
+    float sigmaSquaredNoiseNormed2D;
+    float sigmaSquaredSharpenMinNormed;
+    float sigmaSquaredSharpenMaxNormed;
+    float ht2n; /* halo threshold squared normed */
+    float norm; /* normalization factor */
+
+    std::unique_ptr<float[], decltype(&fftw_free)> pattern2d;
+    std::unique_ptr<float[], decltype(&fftw_free)> pattern3d;
+
+    const VSVideoInfo *vi;
+    VSNodeRef *node;
+
+    template < int btcur >
+    void Wiener3D(int n, VSNodeRef *node, VSFrameRef *dst, VSFrameContext *frame_ctx, const VSAPI *vsapi);
+
+public:
+    VSFrameRef *ApplyFilter(int n, VSFrameContext *frame_ctx, VSCore *core, const VSAPI *vsapi);
+
+    /* Constructor */
+    FFT3DFilter
+    (
+        FFT3DFilterTransform *transform, const VSVideoInfo *vi,
+        float _sigma, float _beta, int _plane, int _bw, int _bh, int _bt, int _ow, int _oh,
+        float _kratio, float _sharpen, float _scutoff, float _svr, float _smin, float _smax,
+        int _pframe, int _px, int _py, bool _pshow, float _pcutoff, float _pfactor,
+        float _sigma2, float _sigma3, float _sigma4, float _degrid,
+        float _dehalo, float _hr, float _ht, int _ncpu,
+        VSNodeRef *node, VSCore *core, const VSAPI *vsapi
+    );
 
     static const VSFrameRef *VS_CC GetFrame(int n, int activation_reason, void *instance_data, void **frame_data, VSFrameContext *frame_ctx, VSCore *core, const VSAPI *vsapi);
     static void VS_CC Free(void *instance_data, VSCore *core, const VSAPI *vsapi);
